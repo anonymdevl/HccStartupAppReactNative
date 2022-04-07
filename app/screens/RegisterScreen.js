666ef6f043a66e365cf4297024432ff1e769cf7b
Michael Appiah-Kubi <kubiappiahmichael@gmail.com>
2022-04-07 17:01:26 +0000

    Created Screen components for my screens

---
+++ app/screens/RegisterScreen.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { StyleSheet, Image } from "react-native";
+import * as Yup from "yup";
+
+import Screen from "../components/Screen";
+import {
+  AppForm as Form,
+  AppFormField as FormField,
+  SubmitButton,
+} from "../components/forms";
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required().label("Name"),
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+  team: Yup.string().required().label("Teams"),
+});
+
+function RegisterScreen() {
+  return (
+    <Screen style={styles.container}>
+      <Image style={styles.logo} source={require("../assets/cepe.png")} />
+
+      <Form
+        initialValues={{ name: "", email: "", password: "", team: "" }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        <FormField
+          autoCorrect={false}
+          icon="account"
+          name="name"
+          placeholder="Name"
+        />
+        <FormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="email"
+          keyboardType="email-address"
+          name="email"
+          placeholder="Email"
+          textContentType="emailAddress"
+        />
+        <FormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="lock"
+          name="password"
+          placeholder="Password"
+          secureTextEntry
+          textContentType="password"
+        />
+        <FormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="account-group"
+          name="team"
+          placeholder="Team"
+        />
+        <SubmitButton title="Register" />
+      </Form>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20,
+  },
+});
+
+export default RegisterScreen;
