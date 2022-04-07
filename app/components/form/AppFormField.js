941f167148e435be678f1f3da974481976562308
Michael Appiah-Kubi <kubiappiahmichael@gmail.com>
2022-04-07 17:00:12 +0000

    Created form components

---
+++ app/components/form/AppFormField.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useFormikContext } from "formik";
+
+import AppTextInput from "../AppTextInput";
+import ErrorMessage from "../form";
+
+function AppFormField({ name, ...otherProps }) {
+  const { setFieldTouched, handleChange, errors, touched } = useFormikContext();
+
+  return (
+    <>
+      <AppTextInput
+        // autoCapitalize='none'
+        // autoCorrect={false}
+        // icon='email'
+        // keyboardType='email-address'
+        onBlur={() => setFieldTouched("name")}
+        onChangeText={handleChange("name")}
+        {...otherProps}
+        // placeholder='Email'
+        // textContentType='emailAddress'
+      />
+
+      <ErrorMessage error={errors[name]} visible={touched[name]} />
+    </>
+  );
+}
+
+export default AppFormField;
